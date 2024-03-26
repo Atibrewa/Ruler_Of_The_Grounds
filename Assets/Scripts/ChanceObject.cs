575f981466675ddb1848e78cf463ab86b0a29dd9
Atibrewa <atibrewa@macalester.edu>
2024-03-26 10:10:07 -0500

    added bg to rep practice

---
+++ Assets/Scripts/ChanceObject.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName ="NewChanceEvent", menuName ="ChanceEvents/EventCard")]
+public class ChanceObject : ScriptableObject
+{
+//     private void eventText;
+//     private void int athletics;
+//     private void int creativity;
+//     private void int math;
+//     private void int language;
+//     private void int reputation;
+ 
+}
