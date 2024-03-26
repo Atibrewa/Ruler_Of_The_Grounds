84378c26f7c2a987458eaf6f7bec5caaba633efe
Atibrewa <atibrewa@macalester.edu>
2024-03-26 09:42:49 -0500

    added playground scene and bgs!

---
+++ Assets/Scripts/ChanceList.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChanceList : MonoBehaviour
+{
+    List<List<string>> events;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void createlist()
+    {
+        // {"Text to show", Athletics, Creativity, Math, Language, Reputation}
+        events = {
+            {}
+        }
+    }
+}
