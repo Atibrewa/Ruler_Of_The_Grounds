13f73b374a23bd0b170a22a32a98b8c04ef5d656
Maggie Faust <mfaust@macalester.edu>
2024-03-28 11:10:35 -0500

    Added Game Over popup

--- Assets/Scripts/ReputationPracticeScripts/StrikesController.cs
+++ Assets/Scripts/ReputationPracticeScripts/StrikesController.cs
@@ -2,11 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class StrikesController : MonoBehaviour {
 
     int strikes;
     public int maxStrikes;
+
+    public UnityEvent gameOver;
 
     // Start is called before the first frame update
     void Start() {
@@ -21,5 +24,8 @@ public class StrikesController : MonoBehaviour {
     public void AddStrike() {
         strikes += 1;
         GetComponent<TMP_Text>().SetText("Strikes: " + strikes);
+        if (strikes >= maxStrikes) {
+            gameOver.Invoke();
+        }
     }
 }
