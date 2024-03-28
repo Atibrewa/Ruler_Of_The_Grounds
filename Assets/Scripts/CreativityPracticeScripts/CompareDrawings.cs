c7cef1524991f18480681377ce49c1f1d00aa9ee
Linda Lor <77794383+LindaLor028@users.noreply.github.com>
2024-03-28 14:41:23 -0500

    Created MiniGame Scoreboard.

--- Assets/Scripts/CreativityPracticeScripts/CompareDrawings.cs
+++ Assets/Scripts/CreativityPracticeScripts/CompareDrawings.cs
@@ -1,26 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class CompareDrawings : MonoBehaviour
 {
 
     public ReferenceSpriteController headRef, earRef, mouthRef;
     public DrawingSpriteController headDraw, earsDraw, mouthDraw;
+    public TextMeshProUGUI scoreLabel;
 
 
     public void Compare() {
         // get reference combination 
         bool match = false;
-        // match = (headRef.randomSpriteIndex == headDraw.index);
-        // match = (earRef.randomSpriteIndex == earsDraw.index);
-        // match = (mouthRef.randomSpriteIndex == mouthDraw.index);
+        int score = 0;
 
         match = (headRef.randomSpriteIndex == headDraw.index) &
                 (earRef.randomSpriteIndex == earsDraw.index) &
                 (mouthRef.randomSpriteIndex == mouthDraw.index);
 
         Debug.Log("The Match results are: " + match);
+
+        if (headRef.randomSpriteIndex == headDraw.index) {
+            score += 1;
+        }
+
+        if (earRef.randomSpriteIndex == earsDraw.index) {
+            score += 1;
+        }
+
+        if (mouthRef.randomSpriteIndex == mouthDraw.index) {
+            score += 1;
+        }
+
+        scoreLabel.text = "Creativity: +" + score;
+        
         // TODO: based on match value trigger an event/animation that will inform user whether or not they got the match. 
         // Get Animation Object and play it (might be related to PlayableDirector)
     }
