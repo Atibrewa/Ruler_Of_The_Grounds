1fd0a961c483fcb855336c437c5fc8c018b5d795
Linda Lor <77794383+LindaLor028@users.noreply.github.com>
2024-04-11 11:56:22 -0500

    Added working timer to Creativity Mini game.

---
+++ Assets/Scripts/CreativityPracticeScripts/ReferenceTimer.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ReferenceTimer : MonoBehaviour
+{
+    // To be attached to a TextMesh Object thingy 
+    public TextMeshProUGUI count;
+    public TextMeshProUGUI directions;
+    public int waitTime = 5;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Start!");
+        count.text = "";
+        StartCoroutine(Wait());
+        Debug.Log("Start finished");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public IEnumerator Wait() {
+        Debug.Log("Called Wait() for timer");
+        int seconds = 0;
+
+        while (seconds <= waitTime) {
+            if (seconds != 0) {
+                count.text = seconds.ToString();
+            }
+            
+    
+            Debug.Log(seconds);
+            yield return new WaitForSeconds(1f);
+            seconds += 1;
+            if (seconds == waitTime) {
+                directions.text = "TIME'S UP!";
+            }
+
+        }
+        yield return new WaitForSeconds(1f);
+        count.text = "";
+    }
+}
