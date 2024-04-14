58e771c75990fc48a0907a14848e286eafdda606
Atibrewa <atibrewa@macalester.edu>
2024-04-14 17:17:38 -0500

    added basic text on hover!

--- Assets/Scripts/ChangeText.cs
+++ Assets/Scripts/ChangeText.cs
@@ -1,18 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class ChangeText : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public TMP_Text text;
 
-    // Update is called once per frame
-    void Update()
+    public void changeToText(string newText)
     {
-        
+        text.text = newText;
     }
 }
