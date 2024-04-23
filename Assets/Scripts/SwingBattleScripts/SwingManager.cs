1b043cdfdeadcd6460e0db02ee99bee233f03463
Linda Lor <77794383+LindaLor028@users.noreply.github.com>
2024-04-23 10:26:01 -0500

    Fixed progress bar and changed player stats back to positive in athletics

--- Assets/Scripts/SwingBattleScripts/SwingManager.cs
+++ Assets/Scripts/SwingBattleScripts/SwingManager.cs
@@ -1,10 +1,48 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SwingManager : MonoBehaviour
 {
+    public SwingController bullySwing;
+    public SwingController playerSwing; 
 
+    public GameObject progressBar;
+    private Transform bullyProgressBar, playerProgressBar;
+
+    private bool finished = false;
+
+
+    public void Start() {
+        bullyProgressBar = progressBar.transform.GetChild(0).gameObject.GetComponent<Transform>();
+        playerProgressBar = progressBar.transform.GetChild(1).gameObject.GetComponent<Transform>();
+    }
+
+    public void Update() {
+        if (!finished) {
+            UpdateProgressBar();
+        }
+        
+    }
+
+    public void UpdateProgressBar() {
+        // convert tiltAngle to be on the scale of 1.0f
+        // now update the bullyProgressBar scale 
+        if (bullySwing.tiltAngle > 0) {
+            bullyProgressBar.localScale = new Vector3(bullySwing.tiltAngle/160.0f, 1,1);
+        }
+
+        if (playerSwing.tiltAngle > 0) {
+             playerProgressBar.localScale = new Vector3(playerSwing.tiltAngle/160.0f, 1,1);
+        }
+       
+        //Debug.Log("player progress bar is going: " + playerSwing.tiltAngle/80.0f);
+    }
+
+    public void TurnOffProgressUpdates() {
+        finished = true;
+    }
 
 
 }
