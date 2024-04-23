122e65528ca238dc05f7a2bc5271ee74e826be5c
Atibrewa <atibrewa@macalester.edu>
2024-04-23 10:45:19 -0500

    edited prefabs using buttons

--- Assets/Scripts/PlayerStatsScripts/StatsCanvasScript.cs
+++ Assets/Scripts/PlayerStatsScripts/StatsCanvasScript.cs
@@ -11,21 +11,12 @@ public class StatsCanvasScript : MonoBehaviour {
     public TMP_Text creativityText;
     public TMP_Text mathematicsText;
     public Statistics stats;
-    // Start is called before the first frame update
-    void Start() {
-        
-    }
 
-    // Update is called once per frame
-    void Update() {
+    public void updateText() {
         athleticsText.SetText("Athletics: " + stats.athletics.ToString());
         reputationText.SetText("Reputation: " + stats.reputation.ToString());
         languageText.SetText("Language: " + stats.language.ToString());
         creativityText.SetText("Creativity: " + stats.creativity.ToString());
         mathematicsText.SetText("Math: " + stats.math.ToString());
-    }
-
-    public void GoBack() {
-        gameObject.SetActive(false);
     }
 }
