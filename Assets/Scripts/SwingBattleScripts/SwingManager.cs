516e417d7c3d5d02ca0cf185de69f728f538c8d2
Linda Lor <77794383+LindaLor028@users.noreply.github.com>
2024-04-24 07:23:14 -0500

    Changed progress bar UI

--- Assets/Scripts/SwingBattleScripts/SwingManager.cs
+++ Assets/Scripts/SwingBattleScripts/SwingManager.cs
@@ -9,14 +9,19 @@ public class SwingManager : MonoBehaviour
     public SwingController playerSwing; 
 
     public GameObject progressBar;
-    private Transform bullyProgressBar, playerProgressBar;
+    // private Transform bullyProgressBar, playerProgressBar;
+    private Image bullyProgressBar, playerProgressBar;
+
 
     private bool finished = false;
 
 
     public void Start() {
-        bullyProgressBar = progressBar.transform.GetChild(1).gameObject.GetComponent<Transform>();
-        playerProgressBar = progressBar.transform.GetChild(2).gameObject.GetComponent<Transform>();
+        // bullyProgressBar = progressBar.transform.GetChild(1).gameObject.GetComponent<Transform>();
+        // playerProgressBar = progressBar.transform.GetChild(2).gameObject.GetComponent<Transform>();
+
+        bullyProgressBar = progressBar.transform.GetChild(1).gameObject.GetComponent<Image>();
+        playerProgressBar = progressBar.transform.GetChild(2).gameObject.GetComponent<Image>();
     }
 
     public void Update() {
@@ -30,11 +35,13 @@ public class SwingManager : MonoBehaviour
         // convert tiltAngle to be on the scale of 1.0f
         // now update the bullyProgressBar scale 
         if (bullySwing.tiltAngle > 0) {
-            bullyProgressBar.localScale = new Vector3(bullySwing.tiltAngle/160.0f, 1,1);
+            // bullyProgressBar.localScale = new Vector3(bullySwing.tiltAngle/160.0f, 1,1);
+            bullyProgressBar.fillAmount +=  (bullySwing.tiltAngle/160.0f);
         }
 
         if (playerSwing.tiltAngle > 0) {
-             playerProgressBar.localScale = new Vector3(playerSwing.tiltAngle/160.0f, 1,1);
+            //  playerProgressBar.localScale = new Vector3(playerSwing.tiltAngle/160.0f, 1,1);
+            playerProgressBar.fillAmount +=  (playerSwing.tiltAngle/160.0f);
         }
        
         //Debug.Log("player progress bar is going: " + playerSwing.tiltAngle/80.0f);
