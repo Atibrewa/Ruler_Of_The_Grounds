b11574fff95e098ad90e20611fc55b03ec56dcfe
Maggie Faust <mfaust@macalester.edu>
2024-04-25 10:52:45 -0500

    Added bell sound

--- Assets/Scripts/BellScript.cs
+++ Assets/Scripts/BellScript.cs
@@ -6,12 +6,15 @@ public class BellScript : MonoBehaviour {
     // Start is called before the first frame update
 
     public Animator animator;
+    AudioSource audio;
 
     bool ringing = false;
 
     void Start() {
+        audio = GetComponent<AudioSource>();
         if (ringing) {
             Debug.Log("Ringing");
+            audio.Play();
             animator.Play("BellRingAnimation");
         }
         else {
