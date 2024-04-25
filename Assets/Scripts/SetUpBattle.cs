0343c0a6c5471f32bf68c6cefe2c913aad9b345c
Linda Lor <77794383+LindaLor028@users.noreply.github.com>
2024-04-25 10:16:23 -0500

    Fixed progress bar and added Minnie dialogue

--- Assets/Scripts/SetUpBattle.cs
+++ Assets/Scripts/SetUpBattle.cs
@@ -5,13 +5,18 @@ using UnityEngine;
 public class SetUpBattle : MonoBehaviour
 {
     public BattleSetUp setUp;
+    
 
     public GameObject SwingBattle;
+    public GameObject MinnieBattle;
     // Start is called before the first frame update
     void Start()
     {
         if (setUp.bullyBattle.Contains("Swing")) {
             SwingBattle.SetActive(true);
+        }
+        if (setUp.bullyBattle.Contains("Minnie")) {
+            MinnieBattle.SetActive(true);
         }
     }
 
