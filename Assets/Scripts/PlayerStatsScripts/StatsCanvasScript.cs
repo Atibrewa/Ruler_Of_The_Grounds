d89cd462eb2cc2ed97acff44a9604c517a29f246
Maggie Faust <mfaust@macalester.edu>
2024-04-28 23:43:12 -0500

    Added Stat Bars

--- Assets/Scripts/PlayerStatsScripts/StatsCanvasScript.cs
+++ Assets/Scripts/PlayerStatsScripts/StatsCanvasScript.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class StatsCanvasScript : MonoBehaviour {
 
@@ -10,13 +11,23 @@ public class StatsCanvasScript : MonoBehaviour {
     public TMP_Text languageText;
     public TMP_Text creativityText;
     public TMP_Text mathematicsText;
+    public Image reputationBar;
+    public Image athleticsBar;
+    public Image creativityBar;
+    public Image languageBar;
+    public Image mathBar;
     public Statistics stats;
 
     public void updateText() {
         athleticsText.SetText("Athletics: " + stats.athletics.ToString());
+        athleticsBar.fillAmount = stats.athletics/50f;
         reputationText.SetText("Reputation: " + stats.reputation.ToString());
+        reputationBar.fillAmount = stats.reputation/50f;
         languageText.SetText("Language: " + stats.language.ToString());
+        languageBar.fillAmount = stats.language/50f;
         creativityText.SetText("Creativity: " + stats.creativity.ToString());
+        creativityBar.fillAmount = stats.creativity/50f;
         mathematicsText.SetText("Math: " + stats.math.ToString());
+        mathBar.fillAmount = stats.math/50f;
     }
 }
