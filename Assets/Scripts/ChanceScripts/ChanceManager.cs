0e7aa90502b09c1ad8ae7a97f718646420fe27e9
Atibrewa <atibrewa@macalester.edu>
2024-04-30 18:39:41 -0500

    working on chance ranges

--- Assets/Scripts/ChanceScripts/ChanceManager.cs
+++ Assets/Scripts/ChanceScripts/ChanceManager.cs
@@ -11,15 +11,27 @@ public class ChanceManager : MonoBehaviour
     public Statistics playerStats;
     public Image imgSpot;
     private ChanceObject curEvent;
+    private int upperLimit;
 
     // Start is called before the first frame update
     void Start()
     {
-        int chanceIndex = Random.Range (0,(objects.Count - 1));
+        int chanceIndex = Random.Range (0, objects.Count);
         curEvent = objects[chanceIndex];
         text.text = curEvent.eventText;
         imgSpot.sprite = curEvent.imgSprite;
         updateStats();
+    }
+
+    void Update()
+    {
+        // need to update this based on the final numbers of categories A-E
+        // int rep = playerStats.reputation;
+        // if (rep > 40) {
+        //     upperLimit = 40;
+        // } else if (rep > 30) {
+        //     upperLimit = 35;
+        // }
     }
 
     void updateStats()
