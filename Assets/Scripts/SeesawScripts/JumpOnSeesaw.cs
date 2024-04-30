f5f84ff5965350868f1034bccdc5b68078529528
Linda Lor <77794383+LindaLor028@users.noreply.github.com>
2024-04-30 09:09:50 -0500

    Got the basic interactions for seesaw battle

--- Assets/Scripts/SeesawScripts/JumpOnSeesaw.cs
+++ Assets/Scripts/SeesawScripts/JumpOnSeesaw.cs
@@ -1,57 +1,84 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class JumpOnSeesaw : MonoBehaviour
 {
     public Statistics stats;
-    public Statistics opponentStats;
+    public ProgressBarManager progressBar;
+    public UnityEvent winEvent; 
 
-    public SpriteRenderer gameSprite;
+
     private float bounceCount;
     private Rigidbody2D body;
-    public ProgressBarManager progressBar;
-
+    private bool running;
+    private bool isPlayer;
+    
 
     // Start is called before the first frame update
     void Start()
     {
         body = GetComponent<Rigidbody2D>();
         bounceCount = 0f;
+        running = true;
+
+        isPlayer = gameObject.name.Contains("Player");
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (bounceCount > 8.0f) {
-            Debug.Log(gameObject.name + " wins!");
+        if (bounceCount > 4.0f) {
+            running = false;
+            winEvent.Invoke();
         }
+        
     }
 
 
     public void OnTriggerEnter2D(Collider2D other)
     {
-        body.velocity = (transform.up * -15.0f);
-        body.mass = 15;
-        bounceCount += (stats.math/50.0f);
-        Debug.Log(gameObject.name + "'s bounceCount: " + bounceCount);
+        if (running) {
+            body.velocity = (transform.up * -15.0f);
+            body.mass = 15;
+            bounceCount += (stats.math/50.0f);
+            Debug.Log(gameObject.name + "'s bounceCount: " + bounceCount);
 
-        SetProgressBar(bounceCount/16.0f);
-
+            SetProgressBar(bounceCount/8.0f);
+        }
     }
 
     public void OnTriggerExit2D(Collider2D other) 
     {
-        body.velocity = (transform.up * 15.0f);
-        body.mass = 1;
+        if (running) {
+            body.velocity = (transform.up * 15.0f);
+            body.mass = 1;
+        }
+        
     }
 
     private void SetProgressBar(float amount) {
-        if (gameObject.name.Contains("Player")) {
+        if (isPlayer) {
             progressBar.SetPlayerProgress(amount);
         }
         else {
             progressBar.SetBullyProgress(amount);
         }
+    }
+
+    public void GoFlyingOff() {
+        running = false;
+
+        body.constraints = RigidbodyConstraints2D.None;
+
+        
+        if (isPlayer) {
+            body.velocity = new Vector3(-20, 10, 0);
+        }
+        else {
+            body.velocity = new Vector3(20, 10, 0);
+        }
+       
     }
 }
