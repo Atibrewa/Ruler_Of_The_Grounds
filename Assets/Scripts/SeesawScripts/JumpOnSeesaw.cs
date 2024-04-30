9f851dd239c0fda8d1773c118ac3dc77ed364524
Linda Lor <77794383+LindaLor028@users.noreply.github.com>
2024-04-30 08:54:27 -0500

    Got basics of seesaw battle working

--- Assets/Scripts/SeesawScripts/JumpOnSeesaw.cs
+++ Assets/Scripts/SeesawScripts/JumpOnSeesaw.cs
@@ -8,22 +8,24 @@ public class JumpOnSeesaw : MonoBehaviour
     public Statistics opponentStats;
 
     public SpriteRenderer gameSprite;
-    private int index = 0;
+    private float bounceCount;
     private Rigidbody2D body;
+    public ProgressBarManager progressBar;
 
 
     // Start is called before the first frame update
     void Start()
     {
         body = GetComponent<Rigidbody2D>();
-
-
+        bounceCount = 0f;
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        if (bounceCount > 8.0f) {
+            Debug.Log(gameObject.name + " wins!");
+        }
     }
 
 
@@ -31,11 +33,25 @@ public class JumpOnSeesaw : MonoBehaviour
     {
         body.velocity = (transform.up * -15.0f);
         body.mass = 15;
+        bounceCount += (stats.math/50.0f);
+        Debug.Log(gameObject.name + "'s bounceCount: " + bounceCount);
+
+        SetProgressBar(bounceCount/16.0f);
+
     }
 
     public void OnTriggerExit2D(Collider2D other) 
     {
         body.velocity = (transform.up * 15.0f);
         body.mass = 1;
+    }
+
+    private void SetProgressBar(float amount) {
+        if (gameObject.name.Contains("Player")) {
+            progressBar.SetPlayerProgress(amount);
+        }
+        else {
+            progressBar.SetBullyProgress(amount);
+        }
     }
 }
