39a0a610b65780cbef5a4012e8a602a683a6debd
Linda Lor <77794383+LindaLor028@users.noreply.github.com>
2024-05-01 21:09:17 -0500

    Got buttons to disable after beating the bully

--- Assets/Scripts/DeactivateCompletedBattleButtons.cs
+++ Assets/Scripts/DeactivateCompletedBattleButtons.cs
@@ -8,28 +8,36 @@ public class DeactivateCompletedBattleButtons : MonoBehaviour
     public BattleSetUp battles;
 
     public Button swingButton, minnieButton, sandcastleButton, seesawButton, structureButton;
-    // Start is called before the first frame update
-    void Start()
-    {
-        List<string> battlesList = battles.ReturnConqueredBattles();
-        for (int i = 0; i < battlesList.Count; i++) {
-            string battle = battlesList[i];
-            if (battle.Contains("Swing")) {
-                swingButton.enabled = false;
-            }
-            else if (battle.Contains("Minnie")) {
-                minnieButton.enabled = false;
-            }
-            else if (battle.Contains("Sandcastle")) {
-                sandcastleButton.enabled = false;
-            }
-            else if (battle.Contains("SeeSaw")) {
-                seesawButton.enabled = false;
-            }
-            else if (battle.Contains("Structure")) {
-                structureButton.enabled = false;
+    private bool done;
 
-            }  
+    void Start() {
+        done = false; 
+    }
+
+    void Update() {
+        if (battles != null && !done) {
+            done = true;
+            List<string> battlesList = battles.ReturnConqueredBattles();
+            for (int i = 0; i < battlesList.Count; i++) {
+                string battle = battlesList[i];
+                if (battle.Contains("Swing")) {
+                    swingButton.enabled = false;
+                }
+                else if (battle.Contains("Minnie")) {
+                    minnieButton.enabled = false;
+                }
+                else if (battle.Contains("Sandcastle")) {
+                    sandcastleButton.enabled = false;
+                }
+                else if (battle.Contains("SeeSaw")) {
+                    seesawButton.enabled = false;
+                }
+                else if (battle.Contains("Structure")) {
+                    structureButton.enabled = false;
+
+                }  
+            }
+
         }
     }
 
